88fdad2020ebfdfccf2ef5ab30be78ce0f726480
Timo <timo.nudel@yahoo.de>
2018-07-04 23:49:36 +0200

    points use zoom

--- lowpolypainter/zoomTransformer.py
+++ lowpolypainter/zoomTransformer.py
@@ -4,7 +4,7 @@ class ZoomTransformer(object):
 
     def __init__(self):
         self.matrix = numpy.identity(3)
-        self.matrix = [[3,0,0],[0,2,0],[0,0,1]]
+        #self.matrix = [[3,0,0],[0,2,0],[0,0,1]]
 
     def ToViewport(self, point):
         return numpy.resize(numpy.matmul(self.matrix, [point[0], point[1], 1]), 2)
