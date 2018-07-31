e90caf49ed7b6a2bec58cde10faff8173daba5b2
Jan Dihlmann <m.dihlmann@googlemail.com>
2018-07-31 15:17:27 +0200

    Triangulation Mesh Integration
    * Mesh now supports triangulation
      Readdress triangulation button to delaunay / canny edges
      Sped up process -> Triangulation takes 7s (better but not perfect)
      Removed unnecessary dependencies mesh / canvas from objects
    
    * Added vertex merging
      Vertices that are placed on the same position are merged together
    
    * Added vertex lookup table
      In mesh bvertices[x][y] returns vertex at that position

--- lowpolypainter/canvas/mesh.py
+++ lowpolypainter/canvas/mesh.py
@@ -1,3 +1,6 @@
+# Python Modules
+import numpy as np
+
 # Local Modules
 from objects.vertex import Vertex
 from objects.edge import Edge
@@ -25,9 +28,13 @@ class Mesh:
         self.edges = []
         self.faces = []
 
+        # Image Vertices Array
+        self.bvertices = np.zeros([self.parent.width, self.parent.height]).tolist()
+
     """ VERTEX """
     def addVertex(self, coords):
-        vert = Vertex(coords, self.parent, self)
+        vert = Vertex(coords, self.parent)
+        self.bvertices[int(coords[0])][int(coords[1])] = vert
         self.vertices.append(vert)
         return vert
 
@@ -36,7 +43,7 @@ class Mesh:
         edge = vert1.getEdge(vert2)
         if edge is not None:
             return edge
-        edge = Edge(vert1, vert2, self.parent, self)
+        edge = Edge(vert1, vert2, self.parent)
         self.edges.append(edge)
         return edge
 
@@ -48,7 +55,7 @@ class Mesh:
 
     """ FACE """
     def addFace(self, edge1, edge2, edge3):
-        face = Face(edge1, edge2, edge3, self.parent, self)
+        face = Face(edge1, edge2, edge3, self.parent)
         self.faces.append(face)
         return face
 
@@ -78,6 +85,53 @@ class Mesh:
         self.parent.canvas.delete(TAG_EDGE)
         self.parent.canvas.delete(TAG_FACE)
         self.parent.selected = None
+
+    def faceToVertexGeneration(self, point1, point2, point3):
+        # Fast way to generate given face by points
+        # Starts at face and generates edges and vertices
+        # Init methodes are not used as they should be used
+        # (!) Point should be array of coordinate
+
+        # Vertecies
+        vert1 = self.bvertices[point1[0]][point1[1]]
+        if vert1 == 0:
+            vert1 = Vertex(point1, self.parent, False)
+            self.bvertices[point1[0]][point1[1]] = vert1
+            self.vertices.append(vert1)
+
+        vert2 = self.bvertices[point2[0]][point2[1]]
+        if vert2 == 0:
+            vert2 = Vertex(point2, self.parent, False)
+            self.bvertices[point2[0]][point2[1]] = vert2
+            self.vertices.append(vert2)
+
+        vert3 = self.bvertices[point3[0]][point3[1]]
+        if vert3 == 0:
+            vert3 = Vertex(point3, self.parent, False)
+            self.bvertices[point3[0]][point3[1]] = vert3
+            self.vertices.append(vert3)
+
+        # Edges
+        edge1 = vert1.getEdge(vert2)
+        if (edge1 == None ):
+            edge1 = Edge(vert1, vert2, self.parent, False)
+            self.edges.append(edge1)
+
+        edge2 = vert2.getEdge(vert3)
+        if (edge2 == None ):
+            edge2 = Edge(vert2, vert3, self.parent, False)
+            self.edges.append(edge2)
+
+        edge3 = vert3.getEdge(vert1)
+        if (edge3 == None ):
+            edge3 = Edge(vert3, vert1, self.parent, False)
+            self.edges.append(edge3)
+
+        # Face
+        face = Face(edge1, edge2, edge3, self.parent, False)
+        face.coords = face.getCoordinates([point1, point2, point3])
+        face.color = face.getColorFromImage()
+        self.faces.append(face)
 
     """ STORE """
     def save(self):
